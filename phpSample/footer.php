e63b6a680235e65dbe41fcbe581edc834a6f8753
ShumilovMikhail <121401444+ShumilovMikhail@users.noreply.github.com>
2023-01-03 12:19:50 +0300

    Add files via upload

---
+++ phpSample/footer.php
@@ -0,0 +1,7 @@
+<footer>
+  <div class="container">
+    <div class="footer-text">
+      © www.gofra-ural.ru 2016, все права защищены
+    </div>
+  </div>
+</footer>
---
+++ phpSample/maps.php
@@ -0,0 +1,23 @@
+<div class="section-map">
+  <div class="map" id="map"></div>
+  <div class="map-block">
+    <div class="map-block__adress">
+      <div class="map-block__title">Адрес офиса</div>
+      <span class="map-block__text">г. Уфа, Проспект октября, 46</span>
+    </div>
+    <div class="map-block__adress-2">
+      <div class="map-block__title">Адрес склада</div>
+      <span class="map-block__text">г. Уфа, ул. Майкопская, 65/2</span>
+    </div>
+    <div class="map-block__contacts">
+      <div class="map-block__title">Тел. отдела продаж:</div>
+      <span class="map-block__text">8 (347) 271-54-28</span> <br>
+      <span class="map-block__text">8 (937) 363-30-00</span>
+    </div>
+    <button class="map-block__button">Заказать звонок</button>
+    <div class="map-block__mail">
+      <div class="map-block__title">Е-mail</div>
+      <span class="map-block__text">urals.karton@gmail.com</span>
+    </div>
+  </div>
+</div>
